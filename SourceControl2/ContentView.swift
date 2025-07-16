d25bd5762f25e83406f7be4dcb1cc59f7968a487
joe <joescholtz@hotmail.com>
2025-07-16 07:57:20 -0500

    Initial Commit

---
+++ SourceControl2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SourceControl2
+//
+//  Created by Joseph Scholtz on 7/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SourceControl2/SourceControl2App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControl2App.swift
+//  SourceControl2
+//
+//  Created by Joseph Scholtz on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SourceControl2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
